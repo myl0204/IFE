4a0687535addc0fe5e61f13c3016ddbea7f07924
notebook myl <272201252@qq.com>
2017-08-01 16:26:20 +0800

    update

--- nodejs/index.js
+++ nodejs/index.js
@@ -4,32 +4,77 @@ const superagent = require('superagent');
 
 var app = express();
 
-var targetUrl = 'office.qq.com'; //目标网站;
-var jsLibraryInput = 'jquery'; //输入的js库,可包含通匹配符'*'
-var jsLibraryArr = jsLibraryInput.split('*');//分割字符串,这里假设只有一个通匹配符
-var jsLibrary = new RegExp(jsLibraryArr[0]+'.'+jsLibraryArr[1]);
+var targetUrls = []; //目标网站;
+var topNumber = 100;// Top100
+var jsLibraryInput = 'jquery.m  .js'; //输入的js库,可包含通匹配符' '(空格)
+var jsLibraryInput = jsLibraryInput.replace(/\s/g,'.');//将输入的通匹配符替换成正则中的通匹配符
+var jsLibrary = new RegExp(jsLibraryInput);
+
+function getRankedSite(number) {
+
+	var pageCount = Math.ceil(number/20);
+	for(var i = 0; i < pageCount; i++) {
+		var count = i==0? '':i;
+		superagent.get('www.alexa.cn/siterank'+count)
+			.end((err,sres)=>{
+				if(err) {
+					return next(err)
+				}
+
+				var $ = cheerio.load(sres.text);
+
+				$('.info-wrap').each(function(index,item) {
+					var $item = $(item),
+						targetUrls.push($item.find('a').text());
+				})
+			})
+	}
+}
+
 app.get('/',(req,res,next)=>{
-	superagent.get(targetUrl)
-		.end((err,sres)=>{
-			if(err) {
-				return next(err)
-			}
+	getRankedSite(30);
+	for(var i = 0; i < targetUrls.length;i++) {
+		superagent.get(targetUrls[i])
+			.end((err,sres)=>{
+				if(err){
+					return next(err)
+				}
+
+				var $ = cheerio.load(sres.text),
+					result = [];
+
+				$('script').each(function(index,item){
+					var $item = $(item)
+					if(jsLibrary.test($item.attr('src'))) {
+						result.push(targetUrls[i])
+					}
+				})
+				res.send(targetUrls[i])
+			})
+	}
+})
+// app.get('/',(req,res,next)=>{
+// 	superagent.get(targetUrl)
+// 		.end((err,sres)=>{
+// 			if(err) {
+// 				return next(err)
+// 			}
 		
 
-			var $ = cheerio.load(sres.text)
+// 			var $ = cheerio.load(sres.text)
 
-			var result ='不存在';
+// 			var result ='不存在';
 
-			$('script').each(function(index,item){
-				var $item = $(item);
-				if(jsLibrary.test($item.attr('src'))) {
-					result = '存在'
-				}
-			})
+// 			$('script').each(function(index,item){
+// 				var $item = $(item);
+// 				if(jsLibrary.test($item.attr('src'))) {
+// 					result = '存在'
+// 				}
+// 			})
 
-			res.send(result)
-		})
-})
+// 			res.send(result)
+// 		})
+// })
 
 app.listen(3000,()=>{
 	console.log('listening')
